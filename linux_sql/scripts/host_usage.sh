91f681f533da3c1ab0ce64dd86a4a2a02abb6a6e
Fadoua Doghmane <doghmanefadoua@gmail.com>
2025-11-13 17:02:20 +0000

    host_usage.sh bash script

---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,44 @@
+#!/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+memory_free=$(vmstat --unit M | tail -1 | awk -v col="4" '{print $col}')
+cpu_idle=$(vmstat | tail -1 | awk '{print $15}')
+cpu_kernel=$(vmstat | tail -1 | awk '{print $14}')
+disk_io=$(vmstat --unit M -d | tail -1 | awk -v col="10" '{print $col}')
+disk_available=$(df -m / | tail -1 | awk '{print $4}')
+
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
+insert_stmt=$(cat <<-END
+INSERT INTO host_usage (
+    timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available
+) VALUES (
+    '$timestamp', $host_id, '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available'
+);
+END
+)
+
+export PGPASSWORD=$psql_password 
+
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
+
+
